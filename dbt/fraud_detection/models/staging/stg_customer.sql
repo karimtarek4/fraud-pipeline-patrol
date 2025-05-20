e3894278dee47b6966daf64a68544f495267c270
Kareem Tarek <er.kareem.tarek@gmail.com>
2025-05-20 19:40:15 +0300

    refactor: fix stg_customer model by removing derived columns and create dim_customer to conform to kimbell dimensional modeling

--- dbt/fraud_detection/models/staging/stg_customer.sql
+++ dbt/fraud_detection/models/staging/stg_customer.sql
@@ -23,32 +23,14 @@ cleaned AS (
         HomeLat AS home_latitude,
         HomeLon AS home_longitude,
         
-        -- Temporal dimensions
-        AccountCreationMonth AS account_creation_month,
-        EXTRACT(YEAR FROM AccountCreationDate) AS account_creation_year,
-        
         -- System fields
         ingestion_date
     FROM source
-),
-
-final AS (
-    SELECT
-        *,
-        -- Derived risk classification based on fraud history
-        CASE 
-            WHEN past_fraud_count > 5 THEN 'High'
-            WHEN past_fraud_count > 2 THEN 'Medium'
-            ELSE 'Low'
-        END AS risk_level,
-        
-        -- Flag accounts with concerning patterns
-        (past_fraud_count > 0) AS has_fraud_history
-    FROM cleaned
 )
 
 -- Final output with all cleaned and derived fields
 SELECT 
+-- list all columns from previous ctes with addition one by one
     customer_id,
     account_creation_date,
     age,
@@ -57,10 +39,6 @@ SELECT
     home_device,
     home_ip, 
     home_latitude,
-    home_longitude,
-    account_creation_month,
-    account_creation_year,
-    risk_level,
-    has_fraud_history,
-    ingestion_date
- FROM final
+    home_longitude
+ FROM 
+    cleaned
